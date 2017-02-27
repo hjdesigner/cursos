84bfadae99ad10e6356f39748063d3d1f330f221
Henrique Rodrigues <hjdesigner23@gmail.com>
2017-02-27 14:02:16 -0300

    Finalizando o curso

--- react-basico/cdc-admin/src/Autor.js
+++ react-basico/cdc-admin/src/Autor.js
@@ -11,9 +11,7 @@ class FormularioAutor extends Component{
     super();
     this.state = {nome:'',email:'',senha:'' };
     this.enviaForm = this.enviaForm.bind(this);
-    this.setNome = this.setNome.bind(this);
-    this.setEmail = this.setEmail.bind(this);
-    this.setSenha = this.setSenha.bind(this);
+    this.salvaAlteracao = this.salvaAlteracao.bind(this);
   }
 
   enviaForm(evento){
@@ -39,24 +37,21 @@ class FormularioAutor extends Component{
     });
   }
 
-  setNome(evento){
-    this.setState({nome:evento.target.value});
+  salvaAlteracao(nomeInput,evento){
+    var campoSendoAlterado = [];
+    campoSendoAlterado[nomeInput] = evento.target.value;
+    this.setState(campoSendoAlterado);
   }
-  setEmail(evento){
-    this.setState({email:evento.target.value});
-  }
-  setSenha(evento){
-    this.setState({senha:evento.target.value});
-  }
+
 
   render(){
     return(
       <div className="pure-form pure-form-aligned">
         <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
 
-          <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome"/>
-          <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email"/>
-          <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha"/>
+          <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.salvaAlteracao.bind(this,'nome')} label="Nome"/>
+          <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.salvaAlteracao.bind(this,'email')} label="Email"/>
+          <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.salvaAlteracao.bind(this,'senha')} label="Senha"/>
 
           <SubmitCustomizado type="submit" nome="Gravar" />
 
