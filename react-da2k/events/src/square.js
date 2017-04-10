15180dcf879512ac1aeeaa1629999ccaf513f703
Henrique Rodrigues <hjdesigner23@gmail.com>
2017-04-09 23:51:13 -0300

    A prop children

---
+++ react-da2k/events/src/square.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import React from 'react';
+
+const Square = () => (
+  <div style={{
+    backgroundColor: '#000000',
+    height: '100px',
+    width: '100px'
+  }} />
+)
+
+export default Square;
