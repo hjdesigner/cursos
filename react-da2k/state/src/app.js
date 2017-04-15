677d3fed12809e440e918c0e9f285cca9845da2a
Henrique Rodrigues <hjdesigner23@gmail.com>
2017-04-15 02:00:25 -0300

    Entendendo arrow function, Stateful vc staless, Lifecycle dos componentes e lificycle: fluxo de montagem / desmontagem

--- react-da2k/state/src/app.js
+++ react-da2k/state/src/app.js
@@ -1,9 +1,53 @@
 'use strict'
 
 import React, { Component } from 'react'
+// import Button from './button'
+// import Square from './square'
+import Timer from './timer'
 
 class App extends Component {
   constructor () {
+    console.log('constructor')
+    super()
+    this.state = {
+      color: 'green',
+      showTimer: true
+    }
+  }
+
+  componentWillMount () {
+    console.log('Quando o componente foi montato')
+  }
+  componentDidMount () {
+    console.log('É execultado logo após o componente ser rederizado')
+  }
+  render () {
+    return (
+      <div>
+        {this.state.showTimer && <Timer />}
+        <button onClick={() => {
+          this.setState({ showTimer: !this.state.showTimer })
+        }}>Show / hide timer</button>
+      </div>
+    )
+  }
+
+  /* render () {
+    console.log('render')
+    return (
+      <div>
+        <Square color={this.state.color}/>
+        {['red', 'green', 'blue'].map((color) => (
+          <Button
+            key={color}
+            handleClick={() => this.setState({ color })}>
+            {color}
+          </Button>
+        ))}
+      </div>
+    )
+  }
+   constructor () {
     super()
     this.state = {
       text: 'Henrique'
@@ -17,7 +61,7 @@ class App extends Component {
           {this.state.text}
       </div>
     )
-  }
+  } */
 }
 
 export default App
