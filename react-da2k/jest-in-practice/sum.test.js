6f90363d7704a22bde32beae4bae36061d9dcb24
Henrique Rodrigues <hjdesigner23@gmail.com>
2017-06-11 15:44:19 -0300

    M2#06 - Jest Code Coverage

--- react-da2k/jest-in-practice/sum.test.js
+++ react-da2k/jest-in-practice/sum.test.js
@@ -1,5 +1,8 @@
 'use strict'
 
-it('Testando jest', () => {
-  
-})
+const expect = require('chai').expect
+const sum = require('./sum')
+
+it('Sum should be a function', () => {
+  expect(sum).to.be.a('function')
+});
